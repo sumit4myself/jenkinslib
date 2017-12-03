18efff493f21763077d7622a8df992f147643d38
Sumit Kumar Sharma <sumit4myself@gmail.com>
2017-12-03 19:04:43 +0530

    no message

--- script/build/archive.sh
+++ script/build/archive.sh
@@ -3,5 +3,5 @@ workspaceDir=$1
 finalName=$2
 apacheLocation=$3
 echo "Archiving ${finalName} at location [ $apacheLocation ]"
-
+echo $(whoami)
 cp  ${workspaceDir}/build/${finalName} $apacheLocation
