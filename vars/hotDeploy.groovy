3c8238ffef99d837c6960d098522b0a8cf566751
Sumit Kumar Sharma <sumit4myself@gmail.com>
2017-12-16 14:02:12 +0530

    no message

--- vars/hotDeploy.groovy
+++ vars/hotDeploy.groovy
@@ -1,4 +1,4 @@
 def call(server, fileName) {
-        sh "ssh jenkins@${server} './download.sh ${fileName}'"
-        sh "ssh jenkins@${server} './start.sh ${fileName}'"
+        sh "ssh -oStrictHostKeyChecking=no ${server} './download.sh ${fileName}'"
+        sh "ssh -oStrictHostKeyChecking=no ${server} './start.sh ${fileName}'"
 }
