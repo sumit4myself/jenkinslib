8719387972511c4bd5aa87a419456c53b3f6d4de
Sumit Kumar Sharma <sumit4myself@gmail.com>
2017-12-16 14:47:09 +0530

    no message

--- vars/hotDeploy.groovy
+++ vars/hotDeploy.groovy
@@ -1,6 +1,5 @@
 def call(server, buildScriptDir, fileName) {
-        sh "scp ${buildScriptDir}/download.sh ${server}:/opt/build/"
-        sh "scp ${buildScriptDir}start.sh ${server}:/opt/build/"
+        sh "scp ${buildScriptDir}/install/*.sh ${server}:/opt/build/"
         sh "ssh -oStrictHostKeyChecking=no ${server} './opt/build/download.sh ${fileName}'"
         sh "ssh -oStrictHostKeyChecking=no ${server} './opt/build/start.sh ${fileName}'"
 }
